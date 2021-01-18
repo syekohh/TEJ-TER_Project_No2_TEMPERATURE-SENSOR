ccc42937b0353e71112c2fc28dc49da119165217
syekohh <54860722+syekohh@users.noreply.github.com>
2021-01-18 14:13:41 -0500

    Cleaned everything up and added inline comments

--- calculator.ino
+++ calculator.ino
@@ -1,43 +1,88 @@
-/* Basic Arduino example code for displaying text on 16x2, 20x4 etc. character LCDs. More info: www.makerguides.com */
-
-// Include the library:
+// include data.h file for all libraries and constants
 #include "data.h"
 
+// create a DHT object
 dht DHT;
 
-int humidity, temperature;
+/*
+* humidity and temperature variables are created
+* in this scope as they are to be manipulated
+*/
+int humidity = 0, temperature = 0;
 
-// Create an LCD object. Parameters: (RS, E, D4, D5, D6, D7):
-LiquidCrystal lcd = LiquidCrystal(2, 3, 4, 5, 6, 7);
+// create an LCD object
+LiquidCrystal lcd = LiquidCrystal(RS, E, D4, D5, D6, D7);
 
-void setup() {
-  // Specify the LCD's number of columns and rows. Change to (20, 4) for a 20x4 LCD:
-  lcd.begin(16, 2);
+// setup code to run once
+void setup()
+{
+  // begin the serial monitor at a 9600 baud rate
   Serial.begin(9600);
-  delay(500);//Delay to let system boot
-  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
-  delay(1000);//Wait before accessing Sensor
+  // set the buzzer pin as OUTPUT
+  pinMode(buzzPin, OUTPUT);
+  // specify the LCD's number of columns and rows
+  lcd.begin(16, 2);
+  // run the boot sequence
+  bootSequence();
 }
 
-void loop() {
+void loop()
+{
 
+  // read the value/input from the DHT temp and humidity sensor
   DHT.read11(dht_apin);
 
+  // set the temperature and humidity variables to read values
   humidity = DHT.humidity;
   temperature = DHT.temperature;
 
-  // Set the cursor on the third column and the first row, counting starts at 0:
+  // set the cursor on the first column and the first row
   lcd.setCursor(0, 0);
-  // Print the string 'Hello World!':
+  // print the humidity values (as a percent (%))
   lcd.print("Humidity:");
   lcd.print(humidity);
   lcd.print("%");
-  // Set the cursor on the third column and the second row:
+  // set the cursor on the third column and the second row:
   lcd.setCursor(0, 1);
+  // print the temperature values (in terms of degrees (C))
   lcd.print("Temperature:");
   lcd.print(temperature);
   lcd.print("C");
-    
-    delay(1000);//Wait 5 seconds before accessing sensor again.
 
+  // temperature is too hot (27 degrees+) set off the beeper
+  if (temperature >= TOO_HOT)
+  {
+    digitalWrite(buzzPin, HIGH);
+    delay(150);
+    digitalWrite(buzzPin, LOW);
+  }
+  // if the temperature is regulated, keep the beeper off
+  else
+  {
+    digitalWrite(buzzPin, LOW);
+  }
+
+  //Wait 1 seconds before accessing sensor again.
+  delay(1000); 
+
+}
+/*
+* a set of beeps (3) totalling 1.95 seconds to
+* allow for the user to know the program begins,
+* also giving time for the display and sensor to boot up 
+*/
+void bootSequence()
+{
+  digitalWrite(buzzPin, HIGH);
+  delay(500);
+  digitalWrite(buzzPin, LOW);
+  delay(150);
+  digitalWrite(buzzPin, HIGH);
+  delay(500);
+  digitalWrite(buzzPin, LOW);
+  delay(150);
+  digitalWrite(buzzPin, HIGH);
+  delay(500);
+  digitalWrite(buzzPin, LOW);
+  delay(150);
 }
--- data.h
+++ data.h
@@ -1,4 +1,18 @@
+// include libraries for display and sensor
 #include "LiquidCrystal.h"
 #include "dht.h"
 
-#define dht_apin A0
+// define constants for pins
+#define dht_apin A0 // analog 0 for the temperature sensor
+#define buzzPin 12 // digital 12 for the button
+
+// define lcd display pins
+#define RS 2
+#define E 3
+#define D4 4
+#define D5 5
+#define D6 6
+#define D7 7
+
+// define the temperature that is "too hot" and will set off the beeper
+#define TOO_HOT 27
